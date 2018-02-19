cf83e32cd86de92089dec21f81afe11d04ed526e
miguelcordovar <miguelcordovar@gmail.com>
2018-02-19 00:44:36 -0500

    nuevo mensaje

--- src/main/java/pe/cordova/app/DemoApp.java
+++ src/main/java/pe/cordova/app/DemoApp.java
@@ -5,6 +5,7 @@ public class DemoApp {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo!!");
+        System.out.println("Hola Mundo esto es nuevo!!");
 
     }
 
